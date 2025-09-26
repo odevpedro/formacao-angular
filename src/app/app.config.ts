d1a96416ebaf1369815de91bb3f8de0fb79d51d4
odevpedro <pedrosschmidt2@gmail.com>
2025-09-26 10:17:32 -0300

    config angular standalone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
+import { routes } from './app.routes'; // se você tiver rotas, senão pode remover
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),                   // se não tiver rotas, pode remover
+    importProvidersFrom(ReactiveFormsModule) // global p/ reactive forms
+  ],
 };
